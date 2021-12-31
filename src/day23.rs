850ded72d3d9d1097e8195b7b18fe2c898377aac
Mitch Souders <mitch.souders@gmail.com>
2021-12-30 22:24:00 -0800

    Day 23 - Part 2

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,6 @@
 use crate::Day;
 use cgmath::{Point2, Vector2};
+use itertools::Itertools;
 use petgraph::algo::dijkstra::dijkstra;
 use petgraph::graph::{NodeIndex, UnGraph};
 #[allow(unused_imports)]
@@ -8,7 +9,7 @@ use std::fmt::Display;
 use std::io::Result as IoResult;
 use std::str::FromStr;
 
-#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
+#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone, Ord, PartialOrd)]
 pub enum Amp {
     Amber,
     Bronze,
@@ -118,7 +119,7 @@ impl FromStr for Space {
 
 #[derive(Debug, Clone)]
 pub struct Burrow {
-    rooms: HashMap<Amp, [Point2<i32>; 2]>,
+    rooms: HashMap<Amp, Vec<Point2<i32>>>,
     burrow: HashMap<Point2<i32>, Space>,
 }
 
@@ -170,12 +171,9 @@ impl Burrow {
             _ => false,
         };
 
-        // Add a test here to find out if it's already in it's room
-        // and doesn't need to move.
-
         if is_room {
             let your_rooms = self.rooms.get(&amp).unwrap();
-            if your_rooms[1] == from
+            if *your_rooms.iter().last().unwrap() == from
                 || (your_rooms.contains(&from)
                     && your_rooms
                         .iter()
@@ -259,19 +257,178 @@ impl FromStr for Burrow {
             .collect::<Vec<_>>();
         rooms.sort_by_key(|p| (p.x, p.y));
 
-        assert_eq!(rooms.len(), 8);
+        let mut groups = rooms.iter().group_by(|p| p.x);
 
-        let mut rooms = [
-            (Amp::Amber, [rooms[0], rooms[1]]),
-            (Amp::Bronze, [rooms[2], rooms[3]]),
-            (Amp::Copper, [rooms[4], rooms[5]]),
-            (Amp::Desert, [rooms[6], rooms[7]]),
-        ]
-        .into_iter()
-        .collect();
+        let mut rooms = groups
+            .into_iter()
+            .map(|(_key, group)| group.cloned().collect::<Vec<_>>())
+            .zip([Amp::Amber, Amp::Bronze, Amp::Copper, Amp::Desert].into_iter())
+            .map(|(x, y)| (y, x))
+            .collect();
 
         Ok(Burrow { rooms, burrow })
     }
+}
+
+fn run(v: Burrow) -> usize {
+    println!("{:?}", v);
+    println!("{}", v);
+
+    let mut ungraph = UnGraph::<Point2<i32>, _>::new_undirected();
+    let node_indices = v
+        .burrow
+        .iter()
+        .map(|(p, _b)| (*p, ungraph.add_node(*p)))
+        .collect::<HashMap<_, _>>();
+
+    node_indices.iter().for_each(|(p, n1)| {
+        let b = v.burrow.get(&p).unwrap();
+        if b.walkable() {
+            let dirs = [
+                Vector2::new(1, 0),
+                Vector2::new(0, 1),
+                Vector2::new(-1, 0),
+                Vector2::new(0, -1),
+            ];
+
+            for d in dirs {
+                let p2 = p + d;
+                if let Some(b2) = v.burrow.get(&p2) {
+                    if b2.walkable() {
+                        let n2 = node_indices.get(&p2).unwrap();
+                        ungraph.add_edge(*n1, *n2, 1);
+                    }
+                }
+            }
+        }
+    });
+
+    let dists = node_indices
+        .iter()
+        .flat_map(|(p, ni)| {
+            v.burrow
+                .get(p)
+                .filter(|b| b.stand())
+                .map(|b| (*p, dijkstra(&ungraph, *ni, None, |_| 1)))
+        })
+        .collect::<HashMap<_, _>>();
+
+    let mut states = HashMap::new();
+
+    fn dfs(
+        states: &mut HashMap<Vec<(Point2<i32>, Amp)>, usize>,
+        burrow: Burrow,
+        node_indices: &HashMap<Point2<i32>, NodeIndex>,
+        graph: &UnGraph<Point2<i32>, usize>,
+        dist: &HashMap<Point2<i32>, HashMap<NodeIndex, usize>>,
+        energy: usize,
+        best: Option<usize>,
+    ) -> Option<usize> {
+        if let Some(b) = best {
+            if energy > b {
+                return None;
+            }
+        }
+        //println!("{:?}", energy);
+        if burrow.all_good() {
+            return Some(energy);
+        }
+        let burrow = &burrow;
+        let mut amps = burrow
+            .burrow
+            .iter()
+            .flat_map(|(p, x)| match x {
+                Space::Floor(amp) | Space::Room(amp) => amp.map(|a| (*p, a)),
+                _ => None,
+            })
+            .collect::<Vec<(Point2<_>, Amp)>>();
+
+        amps.sort_by_key(|(p, a)| (p.x, p.y, *a));
+
+        if states.get(&amps).map(|e| *e < energy).unwrap_or(false) {
+            return None;
+        }
+        states.insert(amps.clone(), energy);
+
+        amps.into_iter()
+            .flat_map(|(amp_pos, amp)| {
+                let legal_endpoint: Vec<_> = burrow
+                    .burrow
+                    .iter()
+                    .filter(|(p, _b)| burrow.can_move(amp, amp_pos, **p))
+                    .collect();
+
+                let mut reachable = HashSet::new();
+
+                petgraph::visit::depth_first_search(
+                    &graph,
+                    Some(node_indices[&amp_pos]),
+                    |event| {
+                        use petgraph::visit::{Control, DfsEvent};
+                        match event {
+                            DfsEvent::Discover(n, _) => {
+                                if graph[n] == amp_pos {
+                                    Control::Continue
+                                } else if burrow.burrow[&graph[n]].blocked() {
+                                    Control::<()>::Prune
+                                } else {
+                                    reachable.insert(n);
+                                    Control::Continue
+                                }
+                            }
+                            _ => Control::Continue,
+                        }
+                    },
+                );
+                //println!("{}", burrow);
+                //println!("{:?} {:?} {:#?}", amp_pos, amp, reachable.iter().map(|n| graph[*n]).collect::<Vec<_>>());
+                //println!("{:#?}", legal_endpoint);
+
+                legal_endpoint
+                    .into_iter()
+                    .filter(move |(p, _b)| reachable.contains(&node_indices[p]))
+                    .map(move |(p, b)| (amp_pos, amp, p, b))
+            })
+            .scan(best, move |mut best, (amp_pos, amp, p, b)| {
+                let mut burrow = burrow.clone();
+                burrow.move_amp(amp_pos, *p);
+                let found = dfs(
+                    states,
+                    burrow,
+                    node_indices,
+                    graph,
+                    dist,
+                    energy
+                        + amp.energy()
+                            * dist.get(&amp_pos).unwrap().get(&node_indices[&p]).unwrap(),
+                    *best,
+                );
+                match (found, &best) {
+                    (Some(f), None) => {
+                        *best = found;
+                    }
+                    (Some(f), Some(b)) if *b > f => {
+                        *best = found;
+                        println!("Best {:?}", best);
+                    }
+                    _ => {}
+                }
+                Some(*best)
+            })
+            .flatten()
+            .min()
+    }
+
+    dfs(
+        &mut states,
+        v.clone(),
+        &node_indices,
+        &ungraph,
+        &dists,
+        0,
+        None,
+    )
+    .unwrap()
 }
 
 pub struct Solution {}
@@ -291,147 +448,12 @@ impl Day for Solution {
         Ok(s.parse().unwrap())
     }
     fn p1(v: &Self::Input) -> Self::Sol1 {
-        println!("{:?}", v);
-        println!("{}", v);
-
-        let mut ungraph = UnGraph::<Point2<i32>, _>::new_undirected();
-        let node_indices = v
-            .burrow
-            .iter()
-            .map(|(p, _b)| (*p, ungraph.add_node(*p)))
-            .collect::<HashMap<_, _>>();
-
-        node_indices.iter().for_each(|(p, n1)| {
-            let b = v.burrow.get(&p).unwrap();
-            if b.walkable() {
-                let dirs = [
-                    Vector2::new(1, 0),
-                    Vector2::new(0, 1),
-                    Vector2::new(-1, 0),
-                    Vector2::new(0, -1),
-                ];
-
-                for d in dirs {
-                    let p2 = p + d;
-                    if let Some(b2) = v.burrow.get(&p2) {
-                        if b2.walkable() {
-                            let n2 = node_indices.get(&p2).unwrap();
-                            ungraph.add_edge(*n1, *n2, 1);
-                        }
-                    }
-                }
-            }
-        });
-
-        let dists = node_indices
-            .iter()
-            .flat_map(|(p, ni)| {
-                v.burrow
-                    .get(p)
-                    .filter(|b| b.stand())
-                    .map(|b| (*p, dijkstra(&ungraph, *ni, None, |_| 1)))
-            })
-            .collect::<HashMap<_, _>>();
-
-        fn dfs(
-            burrow: Burrow,
-            node_indices: &HashMap<Point2<i32>, NodeIndex>,
-            graph: &UnGraph<Point2<i32>, usize>,
-            dist: &HashMap<Point2<i32>, HashMap<NodeIndex, usize>>,
-            energy: usize,
-            best: Option<usize>,
-        ) -> Option<usize> {
-            if let Some(b) = best {
-                if energy > b {
-                    return None;
-                }
-            }
-            //println!("{:?}", energy);
-            if burrow.all_good() {
-                return Some(energy);
-            }
-            let burrow = &burrow;
-            let amps = burrow
-                .burrow
-                .iter()
-                .flat_map(|(p, x)| match x {
-                    Space::Floor(amp) | Space::Room(amp) => amp.map(|a| (*p, a)),
-                    _ => None,
-                })
-                .collect::<Vec<(Point2<_>, Amp)>>();
-
-            amps.into_iter()
-                .flat_map(|(amp_pos, amp)| {
-                    let legal_endpoint: Vec<_> = burrow
-                        .burrow
-                        .iter()
-                        .filter(|(p, _b)| burrow.can_move(amp, amp_pos, **p))
-                        .collect();
-
-                    let mut reachable = HashSet::new();
-
-                    petgraph::visit::depth_first_search(
-                        &graph,
-                        Some(node_indices[&amp_pos]),
-                        |event| {
-                            use petgraph::visit::{Control, DfsEvent};
-                            match event {
-                                DfsEvent::Discover(n, _) => {
-                                    if graph[n] == amp_pos {
-                                        Control::Continue
-                                    } else if burrow.burrow[&graph[n]].blocked() {
-                                        Control::<()>::Prune
-                                    } else {
-                                        reachable.insert(n);
-                                        Control::Continue
-                                    }
-                                }
-                                _ => Control::Continue,
-                            }
-                        },
-                    );
-                    //println!("{}", burrow);
-                    //println!("{:?} {:?} {:#?}", amp_pos, amp, reachable.iter().map(|n| graph[*n]).collect::<Vec<_>>());
-                    //println!("{:#?}", legal_endpoint);
-
-                    legal_endpoint
-                        .into_iter()
-                        .filter(move |(p, _b)| reachable.contains(&node_indices[p]))
-                        .map(move |(p, b)| (amp_pos, amp, p, b))
-                })
-                .scan(best, move |mut best, (amp_pos, amp, p, b)| {
-                    let mut burrow = burrow.clone();
-                    burrow.move_amp(amp_pos, *p);
-                    let found = dfs(
-                        burrow,
-                        node_indices,
-                        graph,
-                        dist,
-                        energy
-                            + amp.energy()
-                                * dist.get(&amp_pos).unwrap().get(&node_indices[&p]).unwrap(),
-                        *best,
-                    );
-                    match (found, &best) {
-                        (Some(f), None) => {
-                            *best = found;
-                        }
-                        (Some(f), Some(b)) if *b > f => {
-                            *best = found;
-                            println!("Best {:?}", best);
-                        }
-                        _ => {}
-                    }
-                    Some(*best)
-                })
-                .flatten()
-                .min()
-        }
-
-        dfs(v.clone(), &node_indices, &ungraph, &dists, 0, None).unwrap()
+        run(v.clone())
     }
-    fn p2(_v: &Self::Input) -> Self::Sol2 {
-        unimplemented!()
+    fn p2(v: &Self::Input) -> Self::Sol2 {
+        let input = std::fs::read_to_string("input/day23p2.txt").unwrap();
+        let input = Solution::process_input(std::io::BufReader::new(input.as_bytes())).unwrap();
+        run(input)
     }
 }
 
@@ -449,6 +471,20 @@ mod test {
         );
         let input = Solution::process_input(std::io::BufReader::new(input.as_bytes())).unwrap();
         assert_eq!(Solution::p1(&input), 12521);
+    }
+    #[test]
+    fn test2() {
+        let input = concat!(
+            "#############\n",
+            "#...........#\n",
+            "###B#C#B#D###\n",
+            "  #D#C#B#A#  \n",
+            "  #D#B#A#C#  \n",
+            "  #A#D#C#A#  \n",
+            "  #########  "
+        );
+        let input = Solution::process_input(std::io::BufReader::new(input.as_bytes())).unwrap();
+        assert_eq!(run(input), 44169);
         //assert_eq!(Solution::p2(&input), 26984457539);
         //unimplemented!()
     }
